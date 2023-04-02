55583e2001614aef22b8f69c91fee0e264d58873
Aneesh013 <aneesh.anil@yahoo.com>
2023-04-02 19:42:12 -0400

    commit new

--- src/Code.js
+++ src/Code.js
@@ -17,12 +17,3 @@ function include(filename) {
       .getContent();
 }
 
-function Search() {//Search
-  let input = document.getElementById('myInput').value
-  input=input.toLowerCase();
-  let x = document.getElementsByTagName('article');
-  for (i = 0; i < x.length; i++) {
-  if(!x[i].innerHTML.toLowerCase().includes(input)){
-          x[i].style.display="none";}           
-      }
-  }
