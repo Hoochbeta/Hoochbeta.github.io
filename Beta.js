3fb25168fafcb13b52b7002879d307ccd2bcc75a
Aneesh013 <aneesh.anil@yahoo.com>
2023-04-03 18:33:35 -0400

    leader

--- Beta.js
+++ Beta.js
@@ -1,9 +1,8 @@
 setInterval(function(){
     let counter= 0;
-    let radio = document.getElementById('radio'+ counter);
+    document.getElementById('radio'+ counter).checked=true;
     counter++;
     if(counter > 6){
         counter = 0;
     }
-    radio.checked=true;
 }, 5000);
