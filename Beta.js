8973c89c09d4c322a120c5082f94bcdf20bbeb62
Aneesh013 <aneesh.anil@yahoo.com>
2023-04-05 00:12:41 -0400

    mobile update

--- Beta.js
+++ Beta.js
@@ -1,11 +1,33 @@
-
+let counter= 0;
 setInterval(function(){
-    let counter= 0;
-    document.getElementById('radio'+ counter).checked=true;
-    counter++;
+    document.getElementById('radio'+ counter).checked = true;
+    counter=counter+1;
+    console.log(counter);
     if(counter > 6){
         counter = 0;
     }
 }, 5000);
 
-
+document.querySelectorAll(".subnavbtn").forEach(function(btn) {
+    btn.addEventListener("click", function() {
+      var subnavContent = this.nextElementSibling;
+      if (subnavContent.style.display === "block") {
+        subnavContent.style.display = "none";
+      } else {
+        subnavContent.style.display = "block";
+      }
+    });
+  });
+  
+  document.querySelector(".menu-btn").addEventListener("click", function() {
+    var navbar = document.querySelector(".navbar");
+    navbar.classList.add("active");
+    this.style.visibility = "hidden";
+  });
+  
+  document.querySelector(".close-btn").addEventListener("click", function() {
+    var navbar = document.querySelector(".navbar");
+    navbar.classList.remove("active");
+    document.querySelector(".menu-btn").style.visibility = "visible";
+  });
+  
